8186bd64723df1b2f9c9bf5af818093d668d716f
lwo <lwo@iisg.nl>
2016-05-25 16:58:19 +0200

    Set the provider as IALHI and allow it to be overwritten with mappings.

--- scripts/edmHope.groovy
+++ scripts/edmHope.groovy
@@ -1,44 +1,3 @@
-/*
-mappingHandler = new JSONMappingHandler(mapping);
-template = new JSONMappingHandler(mapping.getJSONObject("template"));
-
-group = template.getHandlersForPath("/RDF/ProvidedCHO").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-
-group = template.getHandlersForPath("/RDF/WebResource").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-
-group = template.getHandlersForPath("/RDF/Agent").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-
-group = template.getHandlersForPath("/RDF/Place").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-
-group = template.getHandlersForPath("/RDF/TimeSpan").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-
-group = template.getHandlersForPath("/RDF/Concept").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-
-group = template.getHandlersForPath("/RDF/Aggregation").get(0);
-group.setString("minOccurs", "0");
-group.setString("maxOccurs", "-1");
-//children = group.getChildren(); for(child in children) { child.element("minOccurs", "0"); }
-*/
-
-
 //Latest Europeana rights
 rights = template.find("//edm:rights");
 for(Element edmRights : rights) {
@@ -66,8 +25,8 @@ for(Element edmRights : rights) {
 
 //Constant mapping for edm:provider
 edmProvider = template.findFirst("//edm:provider");
-edmProvider.addConstantMapping("Hope");
-edmProvider.setFixed(true);
+edmProvider.addConstantMapping("IALHI");
+edmProvider.setFixed(false);
 
 //Control subject by vocabularies
 subject = cache.duplicate(template.findFirst("//edm:ProvidedCHO/dc:subject").getId());
